ec6cc881de9e8df6bc8da4f87a0e0787a0e7c7be
titansfandrew <drewdulaney5@gmail.com>
2024-12-16 15:15:24 -0600

    updated dashboard

--- dashboard-project/src/components/dashboard.js
+++ dashboard-project/src/components/dashboard.js
@@ -2,6 +2,16 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./../dashboard.css";
 
+function MyComponent() {
+   return (
+     <div style={{ backgroundColor: 'lightblue', height: '100vh' }}>
+       {/* Content of your component */}
+       <h1>Hello, World!</h1>
+     </div>
+   );
+ }
+ 
+
 function Dashboard() {
 
    const headerStyle = {
@@ -24,46 +34,46 @@ function Dashboard() {
 
     return (
       <>
-            <div style={headerStyle}>
-               <div style={{"display":"flex"}}>
-                  <h1 style={{"width":"60%", "text-align":"right"}}>Dashboard</h1>
-                     <span style={{"width":"40%","text-align":"right"}}>
-                        <span>Goodbye! </span>
-                        <Link to="/login">logout</Link>
-                     </span>
+         <div style={{"text-align":"center"}}>     
+               <div style={headerStyle}>
+                  <div style={{"display":"flex"}}>
+                     <h1 style={{"width":"60%", "text-align":"right"}}>Dashboard</h1>
+                        <span style={{"width":"50%","text-align":"right"}}>
+                           <span>Goodbye! </span>
+                           <Link to="/login">logout</Link>
+                        </span>
+                  </div>
+               </div>
+
+            <div style={{overflow:"auto"}}>
+               <div className="menu">
+               <a href="/" onClick={(e)=>handleLinkClick(e,0)}>Github to this Project</a>
+               <a href="/" onClick={(e)=>handleLinkClick(e,1)}>Random Website Generator</a>
+               <a href="/" onClick={(e)=>handleLinkClick(e,2)}>Radnom Fact Generator</a>
+               <a href="/" onClick={(e)=>handleLinkClick(e,3)}>Github Backend API</a>
+            </div>
+               
+            <div className="main">
+               <h2>Hello There!</h2>
+               <p>In this dashboard there will be a couple links, some to the github for this project and some fun ones. (Will have to copy links)</p>
+               {msg}
+            </div>
+               
+            <div className="right">
+               <h2>About</h2>
+               <p>This is a dashboard.</p>
+               <p>Here you can navigate through some different links.</p>
                </div>
             </div>
-
-         <div style={{overflow:"auto"}}>
-            <div className="menu">
-            <a href="/" onClick={(e)=>handleLinkClick(e,0)}>Github to this Project</a>
-            <a href="/" onClick={(e)=>handleLinkClick(e,1)}>Random Website Generator</a>
-            <a href="/" onClick={(e)=>handleLinkClick(e,2)}>Radnom Fact Generator</a>
-            <a href="/" onClick={(e)=>handleLinkClick(e,3)}>Github Backend API</a>
-         </div>
-
-         <div className="main">
-            <h2>Hello There!</h2>
-            <p>In this dashboard there will be a couple links, some to the github for this project and some fun ones. (Will have to copy links)</p>
-            {msg}
-         </div>
-
-         <div className="right">
-            <h2>About</h2>
-            <p>This is a dashboard.</p>
-            <p>Here you can navigate through some different links.</p>
+            <p></p>
+            <div style={{"background-color":"#e5e5e5","text-align":"center","padding":"10px","margin-top":"7px;"}}>
+                  Welcome!
             </div>
          </div>
-         <p></p>
-         <div style={{"background-color":"#e5e5e5","text-align":"center","padding":"10px","margin-top":"7px;"}}>
-               Welcome!
-         </div>
-
       </>
 
     );
   }
   
   export default Dashboard;
-  
   
