b74e6116c32222324646ac3af7523a2914f64f79
titansfandrew <drewdulaney5@gmail.com>
2024-12-16 15:45:54 -0600

    updated homepage

--- dashboard-project/src/components/home.js
+++ dashboard-project/src/components/home.js
@@ -2,10 +2,22 @@ import { Link } from "react-router-dom";
 import "./../home.css";
 
 function Home() {
+
+   const headerStyle = {
+ 
+      backgroundColor:"#e5e5e5",
+      padding:"15px",
+      textAlign:"center",
+      alignItems: "center",
+      justifyContent: "center"
+    }
+
     return (
       <>
          <div style={{"text-align":"center"}}>
-            <h2>Welcome to my dashboard app.</h2>
+            <div style={headerStyle}>
+               <h2>Welcome to my dashboard app.</h2>
+            </div>
             <p>
                <Link to="/login">Go to login page</Link>
             </p>
